ed54430b700bb5a2418e6bf4a4d86686a58e9bb2
zn <currencyAll@163.com>
2018-09-27 16:48:26 +0800

    Spring boot integration of Logback
    我将Logback日志管理整合到了Spring boot中. 分为生产模式和开发模式. 两个模式的切换需要在application.yml中进行.
    I integrated Logback log management into Spring boot. It is divided into production mode and development mode. Switching between the two modes needs to be done in application.yml.

---
+++ src/main/java/com/msnowqueen/mqueen/controller/LogbackTestController.java
@@ -0,0 +1,36 @@
+package com.msnowqueen.mqueen.controller;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by zn* on 2018/9/27
+ */
+@RestController
+@Api(value = "LogBack测试")
+@RequestMapping("/logback")
+@Slf4j
+public class LogbackTestController {
+
+    @ApiImplicitParams({
+            @ApiImplicitParam(paramType = "header", name = "Token",
+                    value = "token", dataType = "String", required = true,
+                    defaultValue = "123")
+    })
+    @ApiOperation(value = "LogBack测试")
+    @RequestMapping(value = "/test", method = RequestMethod.GET)
+    public int logbackTest() {
+        log.info("info配置");
+        log.error("error配置");
+        log.debug("debug配置");
+        log.warn("warn配置");
+        return 1;
+    }
+
+}
